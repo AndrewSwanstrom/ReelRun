87fc1024bc40f83186d8045c7274f4c2f4ad5a40
Zongyang Min <mzongyang@gmail.com>
2025-02-25 14:03:08 -0500

    small update

---
+++ Source/ReelRun/Public/Hello.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Hello.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REELRUN_API UHello : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
