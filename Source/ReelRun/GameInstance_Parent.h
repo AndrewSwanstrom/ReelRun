6f7a341f56121af8445a97a1051da150c8a118b6
Zongyang Min <mzongyang@gmail.com>
2025-02-27 00:55:31 -0500

    c++ Test

---
+++ Source/ReelRun/GameInstance_Parent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameInstance_Parent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REELRUN_API UGameInstance_Parent : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
