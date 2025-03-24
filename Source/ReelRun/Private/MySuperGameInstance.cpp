90b84dda93ad113da96fa9efffffabb81bf5c7cd
Zongyang Min <mzongyang@gmail.com>
2025-03-24 11:17:13 -0400

    more loading screen progress

--- Source/ReelRun/Private/MySuperGameInstance.cpp
+++ Source/ReelRun/Private/MySuperGameInstance.cpp
@@ -2,4 +2,38 @@
 
 
 #include "MySuperGameInstance.h"
+#include "MoviePlayer.h"
+#include "Blueprint/UserWidget.h"
+
+void UMySuperGameInstance::Init()
+{
+	Super::Init();
+
+	FCoreUObjectDelegates::PreLoadMap.AddUObject(this, &UMySuperGameInstance::BeginLoadingScreen);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UMySuperGameInstance::EndLoadingScreen);
+}
+
+void UMySuperGameInstance::BeginLoadingScreen(const FString& MapName)
+{
+	if (GetMoviePlayer() -> IsMovieCurrentlyPlaying())
+	{
+		return;
+	}
+
+	if (!IsRunningDedicatedServer())
+	{
+		FLoadingScreenAttributes LoadingScreen;
+		LoadingScreen.bAutoCompleteWhenLoadingCompletes = false;
+		LoadingScreen.MinimumLoadingScreenDisplayTime = 5.0f;
+		UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), LoadingScreenWidget);
+		LoadingScreen.WidgetLoadingScreen = Widget -> TakeWidget();
+
+		GetMoviePlayer() -> SetupLoadingScreen(LoadingScreen);
+	}
+}
+
+void UMySuperGameInstance::EndLoadingScreen(UWorld* InLoadedWorld)
+{
+	
+}
 
